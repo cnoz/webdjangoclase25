8f97d2ec8a2b8bf3af819c19a810b60288c3ee00
Cristian H. M. Nozralah <cnoz2001@yahoo.com.ar>
2022-09-08 21:53:43 -0300

    inicializacion proyecto webdjango

---
+++ webdjango/view.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from django.template import loader
+from appcoder.models import Curso
+from appcoder.models import Estudiante
+
+
+def home(self, name):
+    return HttpResponse(f'Hola soy {name}')
+
+def homePage(self):
+    lista = [1,2,3,4,5,6,7,8,9]
+    data = {'nombre': 'Derick', 'apellido': 'Carcamo', 'lista' : lista}
+    planilla = loader.get_template('home.html')
+    documento = planilla.render(data)
+    return HttpResponse(documento)
+
+def curso1(self):
+    planilla = loader.get_template('cursos.html')
+    documento = planilla.render()
+    return HttpResponse(documento)
+#
+
+def cursos(self,):
+    #planilla = loader.get_template('cursos.html')
+    curso = Curso(nombre="UX/UI", camada="12345")
+    curso.save()
+    
+    documento = f'Curso: {curso.nombre} camada: {curso.camada}' 
+    return HttpResponse(documento)
+
+
+def estudiante(self,):
+    estudiante = Estudiante(nombre="cristian", apellido= "Nozralah", email= "cnoz2001@yahoo.com.ar")
+    estudiante.save()
+    documento= f' Estudiante: {estudiante.nombre} {estudiante.apellido} {estudiante.email}'
+    return HttpResponse(documento)
